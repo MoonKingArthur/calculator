adca7fad8f7d89c1788754474dd5403d5ddb9b0b
bycdiaz <cdiaz86@gmail.com>
2019-02-09 19:39:36 -0500

    linked files. added empty functions.

---
+++ script.js
@@ -0,0 +1,25 @@
+function operate(num1,numb2,operator) {
+
+    
+
+    function add(num1,num2) {
+    return num1 + num2;
+    };
+    
+    function subtract(num1,num2) {
+        
+    };
+    
+    function multiply(num1,num2) {
+        
+    };
+    
+    function divide(num1,num2) {
+        
+    };
+
+    
+};
+
+
+console.log(operate(1,2,+));
