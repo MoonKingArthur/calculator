16a5bb3edeab89d69c7fc63ba1fcadb00bd7d4b0
bycdiaz <bycdiaz8@gmail.com>
2019-02-19 19:48:58 -0500

    Added divs containing buttons. Established top portion of grid in css.

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let add = (num1,num2) => num1 + num2;
 let subtract = (num1,num2) => num1 - num2;
 let multiply = (num1,num2) => num1 * num2;
 let divide = (num1,num2) => num1 / num2;
+
+function operate(operator,num1,num2) {
+    
+}
