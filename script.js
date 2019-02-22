7983801c8d79cb819d930971376b888d0faa5e8e
bycdiaz <bycdiaz8@gmail.com>
2019-02-22 18:09:52 -0500

    Added switch statement in operator. Added event listener for display div

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 const buttons = document.querySelectorAll('button');
-
-function operate(operator,num1,num2) {
-    let add = (num1,num2) => num1 + num2;
-    let subtract = (num1,num2) => num1 - num2;
-    let multiply = (num1,num2) => num1 * num2;
-    let divide = (num1,num2) => num1 / num2;
-
-    
-}
+const calcDisplay = document.querySelector("#display")
 
 calcInit();
 
@@ -15,10 +7,33 @@ function calcInit() {
     buttons.forEach(button => {
         button.addEventListener('click', selections)
     });
+
+    operate();
 }
 
+function operate(num1,operator,num2) {
+
+  switch (operator) {
+    case '/':
+      return Number(num1) / Number(num2);
+    case '*':
+      return Number(num1) * Number(num2);
+    case '-':
+      return Number(num1) - Number(num2);
+    case '+':
+      return Number(num1) + Number(num2);
+    // default:
+    //   console.log("Sorry, we don't support " + operator + '.');
+  }
+}
+
+// console.log(operate(10,"+",2));
+
+
+// prints clicked buttons to console
 function selections(event) {
-    let userSelection = event.target.innerText;
-    console.log(userSelection);
-    // return userSelection;
+
+  let userSelection = event.target.innerText;    
+  console.log(userSelection);
+  return userSelection;
 }
