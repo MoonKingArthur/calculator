53bd3f98afc89f128e3ee0fc38f489fa36a4deb6
bycdiaz <bycdiaz8@gmail.com>
2019-03-05 21:46:45 -0500

    added code in early return pattern for = to continue operating if another operator, after num1, num2, and operator, is selected.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let num1 = '';
 let num2 = '';
 let operator = '';
 let currentSelection = '';
-let selections = [];
+
 
 calcInit();
 
@@ -12,18 +12,15 @@ function calcInit() {
   buttons.forEach(button => {
     button.addEventListener('click', (event)=>{
       currentSelection = event.target.innerText;
-      selections.push(currentSelection);
       console.log(currentSelection);
       varAssignment(currentSelection);
     })
   });
-    
 }
 
 function operate(num1,operator,num2) {
   // console.log("outcome of logging operator is: " + operator);
   // console.log("outcome of logging num2 is: " + num2);
-  // // num2 is returning as num2=
 
   switch (operator) {
     case '/':
@@ -34,49 +31,45 @@ function operate(num1,operator,num2) {
       return Number(num1) - Number(num2);
     case '+':
       return Number(num1) + Number(num2);
-    // default:
-    //   console.log("Sorry, we don't support " + operator + '.');
   }
 
 }
 
 function varAssignment(currentSelection) {
 
+  // early return pattern
   if (currentSelection === "=") {
-    return displayInfo();
+    num1 = operate(num1,operator,num2);
+    num2 = '';
+    operator = '';
+    // Re-assignes displayArray with a new array with num1 in the 0 index position
+  } 
+  else if (currentSelection === "C") {
+    // TODO clears DOM visually but data is still there.
+    return calcDisplay.innerHTML = "";
+  } 
+  else if (currentSelection.match(/\*|-|\+|\//)) {
+    if (num2) {
+      num1 = operate(num1,operator,num2);
+      num2 = '';
+      operator = '';
+      // Re-assignes displayArray with a new array with num1 in the 0 index position
+    } else if (num1) {
+      operator = currentSelection;
+		}
+  } else if (num1 && operator) {
+      num2 += currentSelection;
+  } else {
+      num1 += currentSelection;
   }
 
-  // TODO clears DOM visually but data is still there.
-  if (currentSelection === "C") {
-    return calcDisplay.innerHTML = "";
-  }
-  
-  if (currentSelection.match(/\*|-|\+|\//)) {
-		if (num1) {
-	    operator = currentSelection;
-		}
-    } else if (num1 && operator) {
-      num2 += currentSelection;
-    } else {
-      num1 += currentSelection;
-    }
-    displayInfo();
-    
+  displayInfo();
+
 }
 
 function displayInfo() {
 	calcDisplay.innerHTML = "";
   const info = document.createElement('p');
-	info.innerText = `${num1} ${operator} ${num2}`;
+  info.innerText = `${num1} ${operator} ${num2}`;
   calcDisplay.appendChild(info);
-
-  if (currentSelection === "=") {
-    calcDisplay.innerHTML = "";
-    const info = document.createElement('p');
-    // console.log(typeof operate(num1,operator,num2));
-    
-    info.innerText = operate(num1,operator,num2);
-    calcDisplay.appendChild(info);
-  }
 }
-
